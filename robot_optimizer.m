eceaa040da0495e7492be4b8e49ae82c81e9489d
Zach Renwick <zrenwick@umich.edu>
2016-02-15 16:38:13 -0500

    minor updates to robot_optimizer.m

--- robot_optimizer.m
+++ robot_optimizer.m
@@ -11,6 +11,5 @@ function [c, opt_L] = robot_optimizer(x, y, dxdt, dydt, d2xdt2, d2ydt2)
     arm_consts = @(L) armConstraints(L, x, y, dxdt, dydt, d2xdt2, d2ydt2);
     
     [opt_L, opt_fval, exit_flag, output] = fmincon(torq_obj, time_o,[],[],[],[],0,[], arm_consts, options);
-    
-
+    c = output.constrviolation;
 end
