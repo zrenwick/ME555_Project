0d7fdf4c3646b06f08e3349b2e8ebcda848435d3
Zachary Benjamin Renwick <zrenwick@eecs4440p06.engin.umich.edu>
2016-02-19 18:14:08 -0500

    fixed robot_optimizer by passing on torque contraint violations

--- robot_optimizer.m
+++ robot_optimizer.m
@@ -5,14 +5,16 @@ function [c, opt_L, avg_torque] = robot_optimizer(x, y, dxdt, dydt, d2xdt2, d2yd
     options = optimoptions('fmincon');
     options.Display = 'none';
     
-    L_o = [15,15]; % initial guess at robot link lengths
-    torq_obj2 = @(L1,L2) avgTorque([L1,L2], x, y, dxdt, dydt, d2xdt2, d2ydt2);
+    L_o = [10,10]; % initial guess at robot link lengths
+    %torq_obj2 = @(L1,L2) avgTorque([L1,L2], x, y, dxdt, dydt, d2xdt2, d2ydt2);
     torq_obj = @(L) avgTorque(L, x, y, dxdt, dydt, d2xdt2, d2ydt2);
     arm_consts = @(L) armConstraints(L, x, y, dxdt, dydt, d2xdt2, d2ydt2, torq_obj);
-    [L1,L2] = meshgrid(0:0.005:7,0:0.005:7);
-    z = arrayfun(torq_obj2,L1,L2);
-    mesh(L1,L2,z);
+    %[L1,L2] = meshgrid(0:0.005:7,0:0.005:7);
+    %z = arrayfun(torq_obj2,L1,L2);
+    %mesh(L1,L2,z);
     [opt_L, avg_torque, exit_flag, output] = fmincon(torq_obj, L_o,[],[],[],[],[0,0],[], arm_consts, options);
-    keyboard
-    c = output.constrviolation;
+    c_raw = arm_consts(opt_L);
+    c = c_raw(2:end);
+    %c = output.constrviolation;
+    %disp(['optimal L: ',num2str(opt_L),'  C: ',num2str(max(c))]);
 end
