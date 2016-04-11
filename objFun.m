99ac0c10f2282ae77b5e36db2b99fd671998f3f6
Zachary Benjamin Renwick <zrenwick@eecs4440p18.engin.umich.edu>
2016-04-11 13:55:55 -0400

    Updated for final sequential run

--- objFun.m
+++ objFun.m
@@ -13,35 +13,44 @@ function reward = objFun(perm_num, n_points, x_coords, y_coords)
     accelcon = @(X) accelConstraint(X, ordered_x_coords, ordered_y_coords); 
     selectorfunc = @(X) X(5);
     
-    options = optimoptions('fmincon','Display','iter','Algorithm','sqp','DiffMinChange',0.1,'TolFun',0.01,'UseParallel',false,'MaxFunEvals',1000);
+    %options = optimoptions('fmincon','Display','iter','Algorithm','sqp','DiffMinChange',0.1,'TolFun',0.01,'UseParallel',false,'MaxFunEvals',1000);
     
     pop_size = 100;
-    pop_matrix = [rand(pop_size,1).*5 + 5, rand(pop_size,1).*5 + 5, zeros(pop_size,1), zeros(pop_size,1), rand(pop_size,1).*50 + 75];
+    %pop_matrix = [rand(pop_size,1).*5 + 5, rand(pop_size,1).*5 + 5, zeros(pop_size,1), zeros(pop_size,1), rand(pop_size,1).*50 + 75];
+    pop_matrix = [];
+    while(size(pop_matrix,1)<pop_size)
+        xc = rand(2,1).*10;
+        vecs = [ordered_x_coords;ordered_y_coords] - xc*ones(1,n_points);
+        lengths = sqrt(vecs(1,:).^2 + vecs(2,:).^2);
+        L_min = min(lengths);
+        L_max = max(lengths);
+        L_1_plus_L_2 = rand.*10 + L_max;
+        L_1_minus_L_2 = sign(rand-0.5)*(rand.*L_min);
+        L1 = (L_1_plus_L_2 + L_1_minus_L_2)/2;
+        L2 = (L_1_plus_L_2 - L_1_minus_L_2)/2;
+                
+%         candidate = [rand.*8 + 2, rand.*8 + 2, rand.*10, rand.*10, rand.*60 + 20];
+        candidate = [L1, L2, xc(1), xc(2), rand.*60 + 20];
+        if(all(accelcon(candidate)<=0))
+            pop_matrix(end+1,:) = candidate;
+        end
+        disp(size(pop_matrix,1))
+    end
     
     %[opt_X, opt_fval, exit_flag, output] = fmincon(selectorfunc, X_o,[],[],[],[],[0;0;0;0;0],[Inf;Inf;0;0;Inf], accelcon, options);
     opts = gaoptimset(...
     'PopulationSize',pop_size,...
-    'Generations', 200,...
+    'Generations', 100,...
     'EliteCount',10,...
-    'TolFun',1e-17,...
+    'TolFun',1e-5,...
+    'TolCon', 1e-8,...
     'InitialPopulation', pop_matrix,...
-    'PlotFcns', @gaplotbestf,...
-    'StallGenLimit', 500,...
+    'StallGenLimit', 50,...
     'Display','iter');
 
     
     
-    [opt_X, opt_fval, exit_flag] = ga(selectorfunc, 5, [],[],[],[], [0;0;0;0;0], [Inf;Inf;0;0;Inf], accelcon, [], opts);
-    keyboard
+    [opt_X, opt_fval, exit_flag, output, population, scores] = ga(selectorfunc, 5, [],[],[],[], [0;0;-Inf;-Inf;0], [Inf;Inf;Inf;Inf;Inf], accelcon, [], opts);
     
-%     pp = cscvn([x_coords(1),x_red(order); y_coords(1), y_red(order);[0,times]]);
-%     der = fnder(pp);
-%     dder = fnder(der);
-%     poses = fnval(pp, pp.coefs);
-%     vels = fnval(der, pp.coefs);
-%     accels = fnval(dder, pp.coefs);
-%     t_vals = linspace(pp.breaks(1),pp.breaks(end),101);
-%     len = sum(sqrt(sum(diff(fnval(pp,t_vals),1,2).^2,1)));
-%     reward = len;
     reward = opt_X(5); % to minimize time
 end
---
+++ objFun_Original.m
@@ -0,0 +1,31 @@
+function reward = objFun_Original(perm_num, n_points, x_coords, y_coords)
+    order = nthperm([1:n_points],perm_num);
+    ordered_x_coords = x_coords(order);
+    ordered_y_coords = y_coords(order);
+    a_max = 10;
+
+    pp = cscvn([ordered_x_coords; ordered_y_coords]);
+    der = fnder(pp);
+    dder = fnder(der);
+    
+    %Calculation of maximum achievable trajectory velocity 
+    % (ds/dt assumed constant throughout trajectory) [d2s/dt2 = 0]
+    % dx/dt = (dx/ds)(ds/dt);
+    % dy/dt = (dy/ds)(ds/dt);
+    % d2x/dt2 = (dx/ds)(d2s/dt2) + (d2x/ds2)(ds/dt)^2
+    % d2x/dt2 = (d2x/ds2)(ds/dt)^2;                     [with d2s/dt2 = 0]
+    % d2y/dt2 = (dy/ds)(d2s/dt2) + (d2y/ds2)(ds/dt)^2
+    % d2y/dt2 = (d2y/ds2)(ds/dt)^2;                     [with d2s/dt2 = 0]
+    
+    poses = fnval(pp, pp.breaks);
+    vels = fnval(der, pp.breaks);
+    accels = fnval(dder, pp.breaks);
+    t_vals = linspace(pp.breaks(1),pp.breaks(end),101);
+    len = sum(sqrt(sum(diff(fnval(pp,t_vals),1,2).^2,1)));
+    [max_curv_mag,pnt_indx] = max(sqrt(accels(1,:).^2 + accels(2,:).^2));
+    
+    
+    t_path = sqrt((len.^2/a_max).*max_curv_mag);
+    
+    reward = t_path;
+end
