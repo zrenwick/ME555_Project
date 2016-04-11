da329a3023ef4906d7e3ec18dc61c6e4611a2c49
Zach Renwick <zrenwick@umich.edu>
2016-04-11 17:00:35 -0400

    added save commands and updated GA settings for first real run.

--- objFun.m
+++ objFun.m
@@ -3,20 +3,12 @@ function reward = objFun(perm_num, n_points, x_coords, y_coords)
     ordered_x_coords = x_coords(order);
     ordered_y_coords = y_coords(order);
     
-    time_o = 50;
-    L_o = [10,10]; % initial guess at robot link lengths
-    xc_o = [0,0];  % initial guess for robot base location
-    X_o = [L_o';xc_o';time_o];
-
-    
     %constraint equation for time optimizer (physical robotic constraints)
     accelcon = @(X) accelConstraint(X, ordered_x_coords, ordered_y_coords); 
     selectorfunc = @(X) X(5);
     
-    %options = optimoptions('fmincon','Display','iter','Algorithm','sqp','DiffMinChange',0.1,'TolFun',0.01,'UseParallel',false,'MaxFunEvals',1000);
-    
     pop_size = 100;
-    %pop_matrix = [rand(pop_size,1).*5 + 5, rand(pop_size,1).*5 + 5, zeros(pop_size,1), zeros(pop_size,1), rand(pop_size,1).*50 + 75];
+    
     pop_matrix = [];
     while(size(pop_matrix,1)<pop_size)
         xc = rand(2,1).*10;
@@ -29,15 +21,12 @@ function reward = objFun(perm_num, n_points, x_coords, y_coords)
         L1 = (L_1_plus_L_2 + L_1_minus_L_2)/2;
         L2 = (L_1_plus_L_2 - L_1_minus_L_2)/2;
                 
-%         candidate = [rand.*8 + 2, rand.*8 + 2, rand.*10, rand.*10, rand.*60 + 20];
         candidate = [L1, L2, xc(1), xc(2), rand.*60 + 20];
         if(all(accelcon(candidate)<=0))
             pop_matrix(end+1,:) = candidate;
         end
-        disp(size(pop_matrix,1))
     end
     
-    %[opt_X, opt_fval, exit_flag, output] = fmincon(selectorfunc, X_o,[],[],[],[],[0;0;0;0;0],[Inf;Inf;0;0;Inf], accelcon, options);
     opts = gaoptimset(...
     'PopulationSize',pop_size,...
     'Generations', 100,...
@@ -45,9 +34,7 @@ function reward = objFun(perm_num, n_points, x_coords, y_coords)
     'TolFun',1e-5,...
     'TolCon', 1e-8,...
     'InitialPopulation', pop_matrix,...
-    'StallGenLimit', 50,...
-    'Display','iter');
-
+    'StallGenLimit', 50);
     
     
     [opt_X, opt_fval, exit_flag, output, population, scores] = ga(selectorfunc, 5, [],[],[],[], [0;0;-Inf;-Inf;0], [Inf;Inf;Inf;Inf;Inf], accelcon, [], opts);
